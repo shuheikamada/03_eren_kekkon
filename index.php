36eb00f23e0ddbbac4e4ec9eccfaa86a2f2ee949
shuhei_kamada <shuhei.kamada.info@gmail.com>
2017-03-15 12:48:27 +0900

    ver.1.0
    ver.1.0

---
+++ index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Cache-Control" content="no-cache">
+<meta name="description" itemprop="description" content="絵恋ちゃん | 結婚しないとナイト">
+<meta name="keywords" itemprop="keywords" content="絵恋ちゃん,結婚しないとナイト,通販">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="cache-control" content="no-cache">
+<meta http-equiv="expires" content="0">
+<meta name="viewport" content="width=device-width, initial-scale=1 ,user-scalable=no">
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<meta name="copyright" content="Copyright 2017 絵恋ちゃん" />
+<!--<meta name="robots" content="noindex">-->
+
+<script src="./js/jquery-3.1.1.min.js"></script>
+
+<title>絵恋ちゃん | 結婚しないとナイト</title>
+<link href="https://fonts.googleapis.com/earlyaccess/roundedmplus1c.css" rel="stylesheet" />
+<link rel="stylesheet" type="text/css" href="./css/common.css?201706200310">
+
+<body id="top">
+  <div class="intro">
+    <div class="copy1"><img src="./img/left.png" class="hand">生まれたからには結婚したい！当たり前の結婚したい！<img src="./img/right.png"class="hand"></div>
+    <div class="copy1tablet">生まれたからには結婚したい！<br>当たり前の結婚したい！</div>
+    <div class="copy1sp">生まれたからには<br>結婚したい！<br>当たり前の<br>結婚したい！</div>
+    <div class="copy2">カリスマアーティスト『絵恋ちゃん』が送る、ゆめにくたらしいシングル10枚目！</div>
+    <div class="logo">
+      <img src="./img/eren-logo.jpg" id="logo1"><br>
+      <img src="./img/album-logo.jpg" id="logo2">
+    </div>
+    <div class="bg">
+      <img src="./img/cover.jpg">
+    </div>
+  </div>
+  <nav class="menu">
+    <div class="container">
+      <div class="menu-button"><a href="#top">ページトップ</a></div>
+      <div class="menu-button"><a href="#details">作品詳細・日程</a></div>
+      <div class="menu-button"><a href="#dinner-show">関連イベント</a></div>
+      <div class="menu-button"><a href="#release-event">リリースイベント</a></div>
+      <div class="menu-button"><a href="#demo">ワンコーラス試聴</a></div>
+    </div>
+  </nav>
+  <div class="sp-menu">
+    menu
+    <div class="menu-list">
+    <div class="menu-close"></div>
+      <ul>
+        <div class="menu-title">menu</div>
+        <a href="#top"><li>top</li></a>
+        <a href="#details"><li>details</li></a>
+        <a href="#distribute"><li>Distribute</li></a>
+        <a href="#crossfade"><li>Crossfade Demo</li></a>
+      </ul>
+    </div>
+  </div>
+<div class="wrapper">
+<div id="details" class="clearfix">
+  <h2><img src="./img/left.png" class="hand">作品詳細・日程<img src="./img/right.png"class="hand"></h2>
+  <div class="cover"><img src="./img/cover.jpg"></div>
+  <div class="track-list">
+	  <ul class="list">
+    <li id="album-title">絵恋ちゃん10thシングル『結婚しないとナイト』</li>
+    <h3>トラックリスト</h3>
+    <li>1.結婚しないとナイト</li>
+    <li>2.気さくが世を斬る</li>
+    <li>3.許容範囲が広い</li>
+    <h3>クレジット</h3>
+    <li>作詞作曲編曲：中村友則</li>
+    <li>ロゴデザイン：さとうたまきらんど(でことらんど) <a href="http://twitter.com/kimatautosa"target=”_blank”>@kimatautosa</li></a>
+    <li>ジャケットヘアメイク：Sumi <a href="http://twitter.com/mut5112"target=”_blank”>@mut5112</li></a>
+    <li>ジャケット撮影：Mei <a href="http://twitter.com/pietorooomei"target=”_blank”>@pietorooomei</li></a>
+    <h3>リリース日程</h3>
+    2017/3/16〜<br>
+    OTOTOY先行ダウンロード販売開始<br>
+    2017/3/23〜<br>
+    そらしどれこーど通販開始<br>
+    ライブ会場にて発売<br>
+    <h3>関連リンク</h3>
+    <a href="http://ototoy.jp/news/85435"target=”_blank”> OTOTOY NEWS 絵恋ちゃん、記念すべき10thシングル『結婚しないとナイト』発売！水野しずとのイベントも決定</a>
+    </ul>
+  </div>
+</div>
+<hr>
+<div id="dinner-show" class="clearfix">
+  <h2><img src="./img/left.png" class="hand">関連イベント<img src="./img/right.png" class="hand"></h2>
+  <div class="dinner-show-flyer"><img src="./img/dinner-show.jpg"></div>
+  <div class="track-list">
+	  <ul class="list">
+    <h3>婚活ディナーショー　婚活しないとナイト　開催</h3>
+    <li>日付：2017/3/18(どようび)</li>
+    <li>場所：名古屋の居酒屋・力雅</li>
+    <li>価格：4000円（コース料理込み）</li>
+    <li>「結婚しないとナイト」初披露</li>
+    </ul>
+  </div>
+</div>
+<hr>
+<div id="release-event" class="clearfix">
+  <h2><img src="./img/left.png" class="hand">リリースイベント<img src="./img/right.png" class="hand"></h2>
+  <div class="cover"><img src="./img/release.jpg"></div>
+  <div class="track-list">
+	  <ul class="list">
+    <h3>リリースイベント開催決定！</h3>
+    <li>2017/4/1 20時～</li>
+    <li>タワーレコード梅田NU茶屋</li>
+    </ul>
+  </div>
+</div>
+<hr>
+<div id="demo" class="clearfix">
+		<h2><img src="./img/left.png" class="hand">ワンコーラス試聴<img src="./img/right.png" class="hand"></h2>
+    <ul>
+      <h3>1.結婚しないとナイト</h3>
+		  <li class="youtubeDummy"><a href="https://m.youtube.com/watch?v=Z0BDyvH75Lo"target=”_blank”><img src="./img/youtube-thumb.jpg"></a></li>
+	  </ul>
+    <ul>
+      <h3>2.気さくが世を斬る</h3>
+		  <li class="youtubeDummy"><a href="https://m.youtube.com/watch?v=P-lCOIwmoOo"target=”_blank”><img src="./img/youtube-thumb.jpg"></a></li>
+	  </ul>
+    <ul>
+      <h3>3.許容範囲が広い</h3>
+		  <li class="youtubeDummy"><a href="https://m.youtube.com/watch?v=YTIhe8tESio"target=”_blank”><img src="./img/youtube-thumb.jpg"></a></li>
+	  </ul>
+</div>
+	</div>
+
+</div>
+<footer>COPYRIGHT © 2017 絵恋ちゃん ALL RIGHTS RESERVED.</footer>
+
+<script type="text/javascript">
+$(document).ready(function() {
+  var ua = navigator.userAgent;
+        var clickoffset = 0;
+  if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0 || ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
+      clickoffset = 50;
+  } else {
+      clickoffset = 100;
+  }
+
+    $('a').click(function(e) {
+
+      /* ページ内遷移の場合のみブラウザ標準動作の抑制 */
+      var prophref = $(this).prop("href"), //リンク先を絶対パスとして取得
+      hrefPageUrl = prophref.split("#")[0], //リンク先を絶対パスについて、#より前のURLを取得
+      currentUrl = location.href, //現在のページの絶対パスを取得
+      currentUrl = currentUrl.split("#")[0]; //現在のページの絶対パスについて、#より前のURLを取得
+      if(hrefPageUrl == currentUrl){
+          e.preventDefault();
+      }
+
+        var speed = 500;
+
+        var href= $(this).attr("href");
+
+        var target = $(href == "#" || href == "" ? 'html' : href);
+
+        var position = target.offset().top - clickoffset;
+
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+
+    });
+
+    $(window).scroll(function() {
+
+      // スクロール量を変数に格納
+      var sc = $(this).scrollTop();
+
+      // メニューバースクロール固定
+      if(sc>770){
+          $('.menu').addClass('menuFix');
+      }else{
+          $('.menu').removeClass('menuFix');
+      }
+
+
+    });
+
+	});
+</script>
+</body>
+
+</html>
